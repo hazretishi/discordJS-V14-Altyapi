be2a659d56d4da073dc44bc721944fcba046d14d
Şi <121644025+hazretishi@users.noreply.github.com>
2022-12-30 01:18:19 +0300

    Update shiConfig.js

--- shiConfig.js
+++ shiConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     Bot: {
-        "shiToken": "MTA1NzcyNzk5MzgzOTEwODIyNg.GXQ5-S.JorDB1Z1CCWzsGqeYtIuCHMhBZVXe0tLQsUERc",
+        "shiToken": "",
         "shiClientID": "1057727993839108226"
     }
 }
